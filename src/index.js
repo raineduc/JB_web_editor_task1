477f85681648710edbb945662b92c558f61432cc
raineduc <hramil0120@gmail.com>
2021-04-11 18:40:45 +0300

    Don't highlight a error when typing a word

--- src/index.js
+++ src/index.js
@@ -11,4 +11,29 @@ const codeEditor = CodeMirror(document.body, {
   lineNumbers: true,
 });
 
+let editorContentChanged = false;
+let isOverlayOn = false;
+
+codeEditor.on("change", (codeMirror, { from, to, text, removed, origin }) => {
+  editorContentChanged = true;
+  if (text.some(s => s.search(/\s/) !== -1 || s === "")) {
+    if (isOverlayOn) return;
+    isOverlayOn = true;
+    codeEditor.addOverlay('spell-checker');
+    return;
+  }
+  isOverlayOn = false;
+  codeEditor.removeOverlay('spell-checker');
+});
+
+codeEditor.on("beforeSelectionChange", (codeMirror, { ranges, origin, update }) => {
+  editorContentChanged = false;
+  setTimeout(() => {
+    if (!editorContentChanged && !isOverlayOn) {
+      isOverlayOn = true;
+      codeEditor.addOverlay('spell-checker');
+    }
+  }, 0);
+})
+
 codeEditor.addOverlay('spell-checker');
