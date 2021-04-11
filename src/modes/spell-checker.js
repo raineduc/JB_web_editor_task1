62c2d956de0e7cfea0b58fd2ea96a98c1b7835ff
raineduc <hramil0120@gmail.com>
2021-04-11 21:23:50 +0300

    Add support russian language Implement dictionary selection simple algorithm

--- src/modes/spell-checker.js
+++ src/modes/spell-checker.js
@@ -1,15 +1,25 @@
 import CodeMirror from "codemirror";
 import Typo from "typo-js";
 
-export const defineSpellCheckerMode = (underlyingTokenAnalyzer) => {
-  let isDictionaryLoaded = false;
-  let typoObject = null;
+const heuristicAlphabetRegex = {
+  "ru": /[а-яА-Я0-9-]/g,
+  "en-US": /[\w-]/g,
+};
 
-  loadENDictionary().then((typo) => {
-    isDictionaryLoaded = true;
-    typoObject = typo;
+export const defineSpellCheckerMode = (underlyingTokenAnalyzer) => {
+  const dictionaries = [];
+
+  loadEnUSDictionary().then((typo) => {
+    dictionaries.push(typo);
+    dictionaries[typo.dictionary] = typo;
   });
 
+  loadRUDictionary().then((typo) => {
+    dictionaries.push(typo);
+  });
+
+
+  console.log("au");
 
   CodeMirror.defineMode("spell-checker", (codeMirrorConfig, modeConfig) => {
     return {
@@ -19,7 +29,7 @@ export const defineSpellCheckerMode = (underlyingTokenAnalyzer) => {
           const word = underlyingTokenAnalyzer.extractWordFromStream(stream);
           if (word.length) {
             advancePosition(stream, word.length);
-            if (isDictionaryLoaded && !typoObject.check(word)) {
+            if (dictionaries.length && !wordSpellWithKnownDictionaries(dictionaries, word)) {
               return "error";
             }
             return null;
@@ -31,19 +41,68 @@ export const defineSpellCheckerMode = (underlyingTokenAnalyzer) => {
   });
 };
 
-const loadENDictionary = async () => {
+/*
+  Simple heuristics dictionary selection algorithm
+  Similarity - coefficient between [0, 1], which is determined by the formula coef = n/N
+  where n - count of alphabet regex matched letters
+  N - length of incoming word 
+
+  Check the word if its max coefficient greater than 0.5
+*/    
+const wordSpellWithKnownDictionaries = (dictionaries, word) => {
+  let chosenTypo = null;
+  let maxSimilarity = 0;
+  for (let typo of dictionaries) {
+    const regex = heuristicAlphabetRegex[typo.dictionary];
+    if (!regex) continue;
+    const similarity = calcDictionarySimilarity(typo, word);
+    if (similarity > maxSimilarity) {
+      maxSimilarity = similarity;
+      chosenTypo = typo;
+    }
+  }
+
+  if (maxSimilarity > 0.5) {
+    return chosenTypo.check(word);
+  }
+
+  return true;
+}
+
+const calcDictionarySimilarity = (typo, word) => {
+  const match = word.match(heuristicAlphabetRegex[typo.dictionary]);
+  if (match === null) return 0;
+  const letter_matches_count = match.reduce((acc, s) => acc + s.length, 0);
+  return letter_matches_count / word.length;
+}
+
+const loadEnUSDictionary = async () => {
   const aff_data = await (
     await fetch(
-      "https://cdn.jsdelivr.net/codemirror.spell-checker/latest/en_US.aff"
+      "/dictionaries/en-US/index.aff"
     )
   ).text();
   const dic_data = await (
     await fetch(
-      "https://cdn.jsdelivr.net/codemirror.spell-checker/latest/en_US.dic"
+      "/dictionaries/en-US/index.dic"
     )
   ).text();
-  return new Typo("en_US", aff_data, dic_data);
+  return new Typo("en-US", aff_data, dic_data);
 };
+
+const loadRUDictionary = async () => {
+  const aff_data = await (
+    await fetch(
+      "/dictionaries/ru/index.aff"
+    )
+  ).text();
+  const dic_data = await (
+    await fetch(
+      "/dictionaries/ru/index.dic"
+    )
+  ).text();
+  return new Typo("ru", aff_data, dic_data);
+}
 
 const advancePosition = (stream, n) => {
   for (let i = 0; i < n; i++) {
